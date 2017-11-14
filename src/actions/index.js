13597644efaf6360a11eae4775e915793d952a9c
Lescouarnec Kevin <kevin_lescouarnec@groupevsc.com>
2017-11-14 18:02:12 +0100

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+export const GET_COUNTRIES = 'GET_COUNTRIES'
+export const ERROR_GET_COUNTRIES = 'ERROR_GET_COUNTRIES'
+
+const API_END_POINT = 'http://api.population.io:80/1.0';
+const DEFAULT_PARAMS = '25/today';
+const GET_MORTALITY = 'GET_MORTALITY';
+const ERROR_GET_MORTALITY = 'ERROR_GET_MORTALITY'
+
+export function getCountries(){
+  return (dispatch) => {
+    axios(`${API_END_POINT}/countries`).then((res) => {
+      dispatch({type: GET_COUNTRIES, payload: res.data.countries})
+    }).catch((err) => {
+      dispatch({type: ERROR_GET_COUNTRIES, error: err.response.data.detail});
+    })
+  }
+}
+
+export function getMortality(country){
+  return (dispatch) => {
+    axios(`${API_END_POINT}/mortality-distribution/${country}/male/${DEFAULT_PARAMS}`).then((resMale) => {
+      axios(`${API_END_POINT}/mortality-distribution/${country}/female/${DEFAULT_PARAMS}`).then((resFemale) => {
+        dispatch({type: GET_MORTALITY, payload: {country: country, male : resMale.data.mortality_distribution, female: resFemale.data.mortality_distribution}})
+      })
+    }).catch((err) => {
+      dispatch({type: ERROR_GET_MORTALITY, error: err.response.data.detail});
+    })
+  }
+}
